9172503bc45df2449797ffb0e7f26cb51d41d854
royce777 <marioboom10@gmail.com>
2022-03-12 16:00:57 +0100

    small homepage styling + translations

--- components/MySlider/MySlider.jsx
+++ components/MySlider/MySlider.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import MySliderData from "./MySliderData";
 
-const MySlider = () => {
+const MySlider = ({t}) => {
   return (
       // TODO: Correct image centering
       <Flex justifyContent='center' alignItems='center'>
@@ -15,17 +15,22 @@ const MySlider = () => {
         ))}
         </Carousel>
         <Box width='100%' position='absolute'>
+            <Center>
+            <Text fontSize='50px' color='white' align='center'>
+             {t('home:welcome_msg', {app_name: 'Realtor'})} 
+            </Text>
+            </Center>
             <Center> 
-                <Button height='80px' margin='3px' colorScheme='blackAlpha'>
-                    I WANT TO BUY
+                <Button height='80px' margin='3px' colorScheme='blackAlpha' bgColor='black'>
+                   {t('home:buy_btn')} 
                 </Button>
-                <Button height='80px' margin='3px' colorScheme='blackAlpha'>
-                    I WANT TO RENT
+                <Button height='80px' margin='3px' colorScheme='blackAlpha' bgColor='black'>
+                   {t('home:rent_btn')} 
                 </Button>
             </Center>
             <Center>
-                <Button height='80px' margin='3px' colorScheme='blackAlpha'>
-                    I WANT TO SELL 
+                <Button height='80px' margin='3px' colorScheme='blackAlpha' bgColor='black'>
+                   {t('home:sell_btn')} 
                 </Button>
             </Center>
         </Box>
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,22 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import MySlider from "../components/MySlider/MySlider";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <Box>
-      <MySlider />
+      <MySlider t={t} />
     </Box>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["home"])),
+    },
+  };
 }
