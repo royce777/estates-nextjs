f13c2223bfc0c0c8e8111b00ef978d731a81b959
Royce <marioboom10@gmail.com>
2024-07-24 18:55:52 -0300

    Fix FDM page layout and add new picture

--- pages/fdm.js
+++ pages/fdm.js
@@ -1,31 +1,70 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
-import Image from 'next/image';
 import styles from '../styles/fdm.module.css';
+import {
+  Textarea, Center, Box, Button,
+  FormControl, FormLabel, Input, SimpleGrid,
+  Text, HStack, Icon, VStack, useToast, Image
+} from "@chakra-ui/react";
 
 const FDM = () => {
   const { t } = useTranslation('fdm');
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>{t('title')}</h1>
-      <section className={styles.section}>
-        <h2 className={styles.subheader}>{t('historicalFacts')}</h2>
-        <p>{t('historicalContent')}</p>
-      </section>
-      <section className={styles.section}>
-        <h2 className={styles.subheader}>{t('mainAttractions')}</h2>
-        <p>{t('attractionsContent')}</p>
-      </section>
-      <section className={styles.section}>
-        <h2 className={styles.subheader}>{t('landscapeFeatures')}</h2>
-        <p>{t('landscapeContent')}</p>
-      </section>
-      <div className={styles.images}>
-        <Image src="/images/forte1.jpg" alt="Forte dei Marmi" width={600} height={400} />
-        <Image src="/images/forte2.jpg" alt="Forte dei Marmi" width={600} height={400} />
-      </div>
-    </div>
+    <>
+      <Center>
+        <Box paddingTop="100px" maxWidth="1000px">
+          <Center>
+            <Text fontSize="25px" fontWeight="bold">{t('title')}</Text>
+          </Center>
+
+          <Text padding="15px" fontSize="18px" fontWeight="semibold" fontStyle="italic">{t('historicalFacts')}</Text>
+
+          <Center>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px" maxWidth="1000px">
+              <Center>
+                <Text padding="15px">
+                  {t('historicalContent')}
+                </Text>
+              </Center>
+              <Center padding="15px">
+                <Image src="/images/forte_old.png" height={{ base: "400px", md: "300px" }} objectFit='cover'></Image>
+              </Center>
+            </SimpleGrid>
+          </Center>
+
+          <Text padding="15px" fontSize="18px" fontWeight="semibold" fontStyle="italic">{t('mainAttractions')}</Text>
+
+          <Center>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px" maxWidth="1000px">
+              <Center>
+                <Text padding="15px">
+                  {t('attractionsContent')}
+                </Text>
+              </Center>
+              <Center padding="15px">
+                <Image src="/images/forte1.jpg" height={{ base: "400px", md: "300px" }} objectFit='cover'></Image>
+              </Center>
+            </SimpleGrid>
+          </Center>
+
+          <Text padding="15px" fontSize="18px" fontWeight="semibold" fontStyle="italic">{t('landscapeFeatures')}</Text>
+
+          <Center>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing="20px" maxWidth="1000px">
+              <Center>
+                <Text padding="15px">
+                  {t('landscapeContent')}
+                </Text>
+              </Center>
+              <Center padding="15px">
+                <Image src="/images/forte2.jpg" height={{ base: "400px", md: "300px" }} objectFit='cover'></Image>
+              </Center>
+            </SimpleGrid>
+          </Center>
+        </Box>
+      </Center>
+    </>
   );
 };
 
